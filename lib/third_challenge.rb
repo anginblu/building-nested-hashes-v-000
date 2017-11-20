dd8d42451783d6f2bc64f70e52aada26ceaa5c96
Jacqueline Lee <anginblu@gmail.com>
2017-11-20 23:06:40 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
         },
       :hero => {
         name: "Romeo",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
@@ -27,7 +27,7 @@ def third_challenge
         },
       :heroine => {
         name: "Juliet",
-        age: "15"
+        age: "15", 
         status: "alive"
         },
       :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}, {}]
